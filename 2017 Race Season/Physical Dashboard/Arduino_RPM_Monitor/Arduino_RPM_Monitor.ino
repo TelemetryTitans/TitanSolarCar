7de733d9aa25320f87eca14d1422032bd479d649
Josef Griesmer <josefgrie@gmail.com>
2017-05-06 17:13:40 -0500

    Organizational
    Added more comments and rearranged variables to make the code more
    readable

--- 2017 Race Season/Physical Dashboard/Arduino_RPM_Monitor/Arduino_RPM_Monitor.ino
+++ 2017 Race Season/Physical Dashboard/Arduino_RPM_Monitor/Arduino_RPM_Monitor.ino
@@ -1,16 +1,19 @@
 //VARIABLES//
+//Pin Variables
+#define interruptPin 2;
+
+//RPM storage and calulation variables
 const int numReadings = 10; //number of readings to sue to average the speed or the car
 int readings[numReadings]; //array with the length of number of readings to be used to average the speed
 unsigned long average = 0;
 int Index = 0; //Index counter for the average array
 unsigned long total; 
-
 volatile int halfRotations = 0; //Rotations counter that is incremneted everytime the hall effect sensor is triggered.
 unsigned long Hz = 0;
-unsigned long lastMillis =  0;
-const byte interruptPin = 2;
-unsigned int count = 20;
+unsigned long lastMillis =  0; //time used to compare to current milliseconds for rpm calculation
+unsigned int count = 20; //number of rotations that the code collects
 
+//Fancy Numbers
 //So every 969.696969 rotations is 1 mile
 //And 16.1616161 rpm is 1 mph
 
@@ -29,9 +32,10 @@ void loop() {
       total = 0;
       readings[Index] = 30 * 1000 / (millis() - lastMillis) * halfRotations;
       //convert Frequency to Rpm and using 30 because half rotations instead of full rotations
-      for (int x = 0; x <= 9; x++) {
-        total = total + readings[x];
+      for (int i = 0; i <= 9; i++) {
+        total = total + readings[i];
       }
+      
       average = total / numReadings;
       Hz = halfRotations / 2;
       Serial.print("RPM = ");
