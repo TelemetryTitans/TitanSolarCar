8f501b3f45fe0bfb3df40b10ac73f8a9d52da73e
salad <salazarjairojohn@gmail.com>
2017-05-07 01:07:05 -0500

    added gitignore
    no more node_modules

--- 2017 Race Season/Digital_Dashboard/Node_Dash/dash.js
+++ 2017 Race Season/Digital_Dashboard/Node_Dash/dash.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var serialport = require('serialport');
-//var SerialPort = serialport.SerialPort;
+
 
 
 if(process.argv[2]){
@@ -11,7 +11,7 @@ if(process.argv[2]){
   });
 
 }
-console.log("connected serialport to "+portname1);
+
 var app = express();
 var port = 1000;
 
