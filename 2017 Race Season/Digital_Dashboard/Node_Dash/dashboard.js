c5abed25b640c2ece859e8fe42153b577c15b29e
salad <salazarjairojohn@gmail.com>
2017-05-24 00:41:15 -0500

    now with csv data logging
    converts json to csv for logging data

--- 2017 Race Season/Digital_Dashboard/Node_Dash/dashboard.js
+++ 2017 Race Season/Digital_Dashboard/Node_Dash/dashboard.js
@@ -5,74 +5,88 @@ var server = require('http').createServer(app)
 var io = require('socket.io').listen(server)
 var SerialPort = require('serialport')
 var usbDetect = require('usb-detection')
+var json2csv = require('json2csv')
+var fs = require('fs')
 
 //variables
-var bmvBool = false;
-var potBool = false;
-var parse;
-var serialdata = {};
-var port = 1000;
+var bmvBool = false
+var potBool = false
+var serialdata = {}
+var port = 1000
+var log = fs.createWriteStream('TelemetryLog.csv', {
+  'flags': 'a'
+})
+
 //functions
-parseBMV = function(line) {
-  var serial = line.split("\t");
+parsebmv = function(line) {
+  var serial = line.split("\t")
 
   switch (serial[0]) {
     case 'V':
-      serialdata.V = Math.floor(serial[1] / 10) / 100;
-      break;
+      serialdata.V = Math.floor(serial[1] / 10) / 100
+      break
     case 'VS':
-      serialdata.VS = Math.floor(serial[1] / 10) / 100;
-      break;
+      serialdata.VS = Math.floor(serial[1] / 10) / 100
+      break
     case 'I':
-      serialdata.I = serial[1];
-      break;
+      serialdata.I = serial[1]
+      break
     case 'SOC':
-      serialdata.SOC = serial[1] / 10;
-      break;
+      serialdata.SOC = serial[1] / 10
+      break
     case 'CE':
-      serialdata.CE = serial[1];
-      break;
+      serialdata.CE = serial[1]
+      break
     case 'VPV':
-      serialdata.VPV = Math.floor(serial[1] / 10) / 100;
-      break;
+      serialdata.VPV = Math.floor(serial[1] / 10) / 100
+      break
     case 'PPV':
-      serialdata.PPV = serial[1];
-      break;
+      serialdata.PPV = serial[1]
+      break
     case 'PID':
-      serialdata.PID = serial[1];
-      break;
+      serialdata.PID = serial[1]
+      break
     case 'H20':
-      serialdata.YT = serial[1];
-      break;
+      serialdata.YT = serial[1]
+      break
     case 'H22':
-      serialdata.YY = serial[1];
-      break;
+      serialdata.YY = serial[1]
+      break
     case 'BMV':
-      serialdata.BMV = serial[1];
-      serialdata.LONG = serial[1];
-      break;
+      serialdata.BMV = serial[1]
+      serialdata.LONG = serial[1]
+      break
   }
 }
+logger = function() {
+  var csv = json2csv({
+    data: serialdata,
+    hasCSVColumnTitle: false,
+    quotes: ''
+  })
+  csv = Date() + "," + csv + "\n"
+  log.write(csv, 'utf8')
+}
 bmv = function(line) {
-  parseBMV(line);
-  io.emit('bmv', serialdata);
+  parsebmv(line)
+  io.emit('bmv', serialdata)
 }
 pot = function(line) {
-  serialdata.turnAngle = line;
-  io.emit('pot', serialdata.turnAngle);
+  serialdata.turnAngle = line
+  io.emit('pot', serialdata.turnAngle)
 }
 dashSetup = function() {
-  app.set('view engine', 'ejs', '');
-  app.use('/assets', express.static('assets'));
+  app.set('view engine', 'ejs', '')
+  app.use('/assets', express.static('middleware'))
   app.get('/', function(req, res) {
-    res.render('sc');
-  });
+    res.render('sc')
+  })
   server.listen(port, function() {
-    console.log('Dashboard launched on port: ' + port);
+    console.log('Dashboard launched on port: ' + port)
     io.on('connection', function(data) {
-      console.log('connected');;
-    });
-  });
+      console.log('connected')
+    })
+  })
 }
 findPorts = function() {
   SerialPort.list(function(err, ports) {
@@ -84,77 +98,79 @@ findPorts = function() {
             var serial = new SerialPort(port.comName, {
               baudRate: 115200,
               parser: SerialPort.parsers.readline('\r\n')
-            });
+            })
             serial.on('data', function(line) {
-              pot(line);
-            });
+              pot(line)
+              logger()
+            })
             serial.on('open', function() {
-              console.log("Arduino Uno connection open on ", port.comName);
-              potBool = true;
-            });
+              console.log("Arduino Uno connection open on ", port.comName)
+              potBool = true
+            })
             serial.on('disconnect', function() {
               serial.close(function(err) {
-                console.log(port.comName, 'serial port disconnected');
-                potBool = false;
-              });
-            });
+                console.log(port.comName, 'serial port disconnected')
+                potBool = false
+              })
+            })
             serial.on('error', function(err) {
               if (err) {
-                console.log('ERROR');
-                console.log('Exiting Dashboard');
-                console.log('Error log: ', err.message);
-                process.exit(1);
+                console.log('ERROR')
+                console.log('Exiting Dashboard')
+                console.log('Error log: ', err.message)
+                process.exit(1)
               }
-            });
+            })
             process.on('uncaughtException', function(err) {
               serial.close(function(err) {
-                console.log(port.comName, 'serial port disconnected');
-                potBool = false;
-              });
-            });
+                console.log(port.comName, 'serial port disconnected')
+                potBool = false
+              })
+            })
           }
-          break;
+          break
         case '6015':
           if (bmvBool == false) {
             var serial = new SerialPort(port.comName, {
               baudRate: 19200,
               parser: SerialPort.parsers.readline('\r\n')
-            });
+            })
             serial.on('data', function(line) {
-              bmv(line);
-            });
+              bmv(line)
+              logger()
+            })
             serial.on('open', function() {
-              console.log("BMV 702 connection open on ", port.comName);
-              bmvBool = true;
-            });
+              console.log("BMV 702 connection open on ", port.comName)
+              bmvBool = true
+            })
             serial.on('disconnect', function() {
               serial.close(function(err) {
-                console.log(port.comName, 'serial port disconnected');
-                bmvBool = false;
-              });
-            });
+                console.log(port.comName, 'serial port disconnected')
+                bmvBool = false
+              })
+            })
             serial.on('error', function(err) {
               if (err) {
-                console.log('ERROR');
-                console.log('Exiting Dashboard');
-                console.log('Error log: ', err.message);
-                process.exit(1);
+                console.log('ERROR')
+                console.log('Exiting Dashboard')
+                console.log('Error log: ', err.message)
+                process.exit(1)
               }
-            });
+            })
           }
-          break;
+          break
         default:
-          break;
+          break
       }
 
 
-    });
-  });
+    })
+  })
 }
 
 //code that fires
-dashSetup();
-findPorts();
+dashSetup()
+findPorts()
 usbDetect.on('add', function(device) {
-  findPorts();
-});
+  findPorts()
+})
